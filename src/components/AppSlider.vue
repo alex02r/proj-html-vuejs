375197605671129739b1d325e670d0b7cf0cb3c5
alex02r <alex.giochi02@gmail.com>
2024-01-17 16:51:33 +0100

    [UPGRADE] Aggiunta struttura html dello slider

--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -4,7 +4,14 @@ export default {
 }
 </script>
 <template lang="">
-    
+    <div class="">
+        <img src="" alt="">
+        <div class="">
+            <h1></h1>
+            <span>Learn cycling from the pros.</span>
+            <button>Learn More -></button>
+        </div>
+    </div>
 </template>
 <style lang="scss" scoped>
     @use '../styles/partial/variables';
