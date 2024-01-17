db3786ea7e504f4917a5f3d421a5bdb43d81bbf9
alex02r <alex.giochi02@gmail.com>
2024-01-17 16:51:11 +0100

    [UPGRADE] Creazione di un nuovo component

---
+++ src/components/AppSlider.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    
+</template>
+<style lang="scss" scoped>
+    @use '../styles/partial/variables';
+    @use '../styles/generals.scss';
+</style>
