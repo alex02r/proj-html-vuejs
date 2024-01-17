731d5ad1bf08a82b3288a9140623d3582b609b36
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-17 19:14:47 +0100

    [UPGRADE] Aggiunto il retro delle card

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -35,24 +35,72 @@ export default {
 
                     <!-- Pacchetto Basic -->
                     <div class="col-3 packages text-center">
-                        <i class="fa-regular fa-handshake"></i>
-                        <h3 class="fw-bolder">Basic</h3>
-                        <p>Learn to ride a bike from the professionals</p>
-                        <ul>
-                            <li v-for="index in li_packs" :key="index">
-                                {{index}}
-                            </li>
-                        </ul>
+
+                        <!-- Fronte della card -->
+                        <div class="front-card">
+                            <i class="fa-regular fa-handshake"></i>
+                            <h3 class="fw-bolder">Basic</h3>
+                            <p>Learn to ride a bike from the professionals</p>
+                            <ul>
+                                <li v-for="index in li_packs" :key="index">
+                                    {{index}}
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Retro della card -->
+                        <div class="retro-card">
+                            <h4 class="text-light text-uppercase fw-bolder">basic</h4>
+                            <p class="fw-bolder">$1800</p>
+                            <button class="fw-bolder">Send Request!</button>
+                        </div>
                     </div>
 
+
                     <!-- Pacchetto Standard -->
-                    <div class="col-3 packages">
-                        
+                    <div class="col-3 packages text-center">
+
+                        <!-- Fronte della card -->
+                        <div class="front-card">
+                            <i class="fa-regular fa-handshake"></i>
+                            <h3 class="fw-bolder">Standard</h3>
+                            <p>Learn to ride a bike from the professionals</p>
+                            <ul>
+                                <li v-for="index in li_packs" :key="index">
+                                    {{index}}
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Retro della card -->
+                        <div class="retro-card">
+                            <h4 class="text-light text-uppercase fw-bolder">Standard</h4>
+                            <p class="fw-bolder">$2500</p>
+                            <button class="fw-bolder">Send Request!</button>
+                        </div>
                     </div>
 
                     <!-- Pacchetto Premium -->
-                    <div class="col-3 packages">
-                        
+                    <div class="col-3 packages text-center">
+
+                        <!-- Fronte della card -->
+                        <div class="front-card">
+                            <i class="fa-regular fa-handshake"></i>
+                            <h3 class="fw-bolder">Premium</h3>
+                            <p>Learn to ride a bike from the professionals</p>
+                            <ul>
+                                <li v-for="index in li_packs" :key="index">
+                                    {{index}}
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Retro della card -->
+                        <div class="retro-card">
+                            <h4 class="text-light text-uppercase fw-bolder">Premium</h4>
+                            <p class="fw-bolder">$5000</p>
+                            <button class="fw-bolder">Send Request!</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -80,40 +128,86 @@ export default {
     }
 
     .packages {
-        background-color: $black;
-        margin: 40px;
-        padding: 20px 10px;
-        border-radius: 5px;
+        height: 500px;
+        margin: 30px;
+        position: relative;
 
-        i {
-            color: $white;
-            font-size: 50px;
+        &:hover .front-card {
+            display: none;
         }
 
-        h3 {
-            margin-top: 30px;
-            color: $white;
-            font-size: 20px;
+        &:hover .retro-card {
+            display: block;
         }
 
-        p {
-            color: $white;
-            font-size: 15px;
-            margin-top: 15px;
-        }
+        .front-card {
+            display: block;
+            width: 100%;
+            height: 100%;
+            background-color: $black;
+            padding: 20px 10px;
+            border-radius: 5px;
 
-        ul {
-            color: $white;
-            list-style: none;
-            margin-top: 30px;
+            i {
+                color: $white;
+                font-size: 50px;
+            }
 
-            li {
+            h3 {
+                margin-top: 30px;
+                color: $white;
+                font-size: 20px;
+            }
+
+            p {
+                color: $white;
                 font-size: 15px;
-                padding: 10px;
-                margin: 0;
-                border-bottom: 1px solid grey;
+                margin-top: 15px;
+            }
+
+            ul {
+                color: $white;
+                list-style: none;
+                margin-top: 30px;
+
+                li {
+                    font-size: 15px;
+                    padding: 10px;
+                    margin: 0;
+                    border-bottom: 1px solid grey;
+                }
+            }
+        }
+
+        .retro-card {
+            background-color: $black;
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: none;
+            width: 100%;
+            height: 100%;
+            padding: 40% 0;
+            border-radius: 5px;
+
+            h3 {
+                margin: 40px;
+            }
+
+            p {
+                color: $white;
+                font-size: 40px;
+            }
+
+            button {
+                margin-top: 50px;
+                padding: 20px;
+                background-color: black;
+                color: $white;
+                border-radius: 5px;
             }
         }
     }
+
 }
 </style>
