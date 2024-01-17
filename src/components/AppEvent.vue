0b515190ecb1ded111f4f7bf68880af7c8f7a8c4
tomasdarienko <darienkotomas@gmail.com>
2024-01-17 17:08:56 +0100

    added top part of event component

--- src/components/AppEvent.vue
+++ src/components/AppEvent.vue
@@ -1,13 +1,29 @@
 <script>
 export default {
-    
+
 }
 </script>
 <template lang="">
-    <div>
-        
+     <div class="container">
+          <div class="row">
+               <div class="col-12">
+                    <div class="text-center">
+                              <h2 class="fw-bolder">Don't Miss Our Events!</h2>
+                              <h4 class="fw-lighter">Get the amazing cycling experience.</h4>
+                    </div>
+
+                    <div class="d-flex justify-content-between mt-5">
+                         <h3 class="fw-lighter">Events</h3>
+                         <div>
+                              <button class="p-1"><i class="fa-solid fa-square"></i></button>
+                              <button class="p-1 "><i class="fa-solid fa-bars"></i></button>
+                              <!-- :class="bg-dark text-light" -->
+                         </div>
+                    </div>
+               </div>
+          </div>
     </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+@use '../styles/generals.scss' as*;
 </style>
