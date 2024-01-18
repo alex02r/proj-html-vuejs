0df5735eb9ce22f27a0f9ff74e3bca78ee43d0e9
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-18 11:34:11 +0100

    created proCycling array

---
+++ src/components/ProfessionalCyclingCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    proCycling: [
+        {
+            image: 'training-box-1.jpg',
+            title: 'Riding Lesson',
+        }, {
+            image: 'training-box-2.jpg',
+            title: 'Safe Driving',
+        }, {
+            image: 'training-box-3.jpg',
+            title: 'Mountain Bike',
+        }, {
+            image: 'training-box-4.jpg',
+            title: 'Trail Drive',
+        }, {
+            image: 'training-box-5.jpg',
+            title: "Pedaling",
+        }
+    ]
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss"></style>
