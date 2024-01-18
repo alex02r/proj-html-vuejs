0f925a7a798e2557df7568dc26c737eaa3f7a1a4
alex02r <alex.giochi02@gmail.com>
2024-01-18 10:04:29 +0100

    [UPGRADE] Struttura html per il footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,8 +4,24 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+            </div>
+            <div class="col-4">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+
+            </div>
+            <div class="col-2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+
+            </div>
+            <div class="col-2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+
+            </div>
+        </div>
     </div>
 </template>
 <style lang="">
