45c823a38caba9e8b5890cb40c38959f19a4d8a9
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-18 11:16:43 +0100

    [FIX] dimensioni colonne header, aggiunto css al dropdown

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,23 +13,23 @@ export default {
 <header>
     <div class='container'>
         <div class="row align-items-center ">
-            <div class="headerCol">
+            <div class="col-3">
                 <div class="logo">
                     <img src="../assets/logo-gobike.png" alt="">
                 </div>
             </div>
-            <div class="headerCol d-flex justify-content-center">
+            <div class="col-6 d-flex justify-content-center">
                 <ul class="m-0 d-flex list-unstyled">
                     <li class="fw-bold mx-1 px-1 fs-5" v-for="(item, index) in menu" :key="index" >
-                        <a class="text-decoration-none text-dark " href="#">{{item}}</a>
+                        <a class="text-decoration-none text-dark" href="#">{{item}}</a>
                     </li>
                     <li class="fw-bold mx-1 px-1 fs-5">
                         <div class="dropdown">
                             <a class="text-decoration-none text-dark" href="#">Trainings</a>
                             <div class="dropdown-content">
-                                <ul>
+                                <ul class="list-unstyled">
                                     <li v-for="(item, index) in trainings" :key="index">
-                                        <a href="#">{{item}}</a>
+                                        <a class="text-decoration-none text-dark" href="#">{{item}}</a>
                                     </li>
                                 </ul>
                             </div>
@@ -37,7 +37,7 @@ export default {
                     </li>
                 </ul>
             </div>
-            <div class="headerCol d-flex justify-content-end ">
+            <div class="col-3 d-flex justify-content-end ">
                 <div class="hamburgerMenu d-flex  align-items-center px-4 fs-2 ">
                     <a class="text-decoration-none text-dark" href="#"><i class="fas fa-bars"></i></a>
                 </div>
@@ -59,52 +59,47 @@ header {
     position: fixed;
     z-index: 1;
 
-    .headerCol {
-        width: calc(100% / 3);
-        height: 100%;
-
-        li {
-            a {
-                position: relative;
-            }
-
-            a::after {
-                content: "";
-                position: absolute;
-                bottom: -8px;
-                height: 3px;
-                width: 100%;
-                left: 0;
-                background-color: black;
-                transition: 0.3s;
-                transition-timing-function: ease-in-out;
-                transform: scaleX(0);
-            }
-
-            a:hover::after {
-                transform: scaleX(1);
-            }
+    li {
+        a {
+            position: relative;
         }
 
-        .dropdown {
-        position: relative;
-        display: inline-block;
+        a::after {
+            content: "";
+            position: absolute;
+            bottom: -8px;
+            height: 3px;
+            width: 100%;
+            left: 0;
+            background-color: black;
+            transition: 0.3s;
+            transition-timing-function: ease-in-out;
+            transform: scaleX(0);
         }
 
-        .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f9f9f9;
-        min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        padding: 12px 16px;
-        z-index: 1;
+        a:hover::after {
+            transform: scaleX(1);
         }
+    }
 
-        .dropdown:hover .dropdown-content {
-        display: block;
-        }
-}
+    .dropdown {
+    position: relative;
+    display: inline-block;
+    }
+
+    .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    padding: 12px 16px;
+    z-index: 1;
+    }
+
+    .dropdown:hover .dropdown-content {
+    display: block;
+    }
     .logo {
         img {
             width: 300px;
