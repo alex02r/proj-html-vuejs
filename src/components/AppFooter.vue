aa1f3e33324fda59b3badfbf3186a5a0ed818b40
alex02r <alex.giochi02@gmail.com>
2024-01-18 10:10:03 +0100

    [UPGRADE] Aggiunto stile generale al Footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,26 +4,38 @@ export default {
 }
 </script>
 <template lang="">
-    <div class="container">
-        <div class="row justify-content-between">
-            <div class="col-2">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
-            </div>
-            <div class="col-4">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
-
-            </div>
-            <div class="col-2">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
-
-            </div>
-            <div class="col-2">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
-
+    <footer class="pt-5">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-2">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+                </div>
+                <div class="col-4">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+    
+                </div>
+                <div class="col-2">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+    
+                </div>
+                <div class="col-2">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam temporibus eum, adipisci maiores quidem dolor quos voluptate quam quae obcaecati explicabo cupiditate itaque impedit dolores consequuntur vero sapiente ea unde!
+    
+                </div>
+                <div class="col-12">
+                    <div class="text-center py-4">
+                        &copy; 2024 Team#2 - Boolean
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </footer>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+    @use '../styles/partial/variables' as *;
+    @use '../styles/generals.scss';
+    footer{
+        background-color: $black;
+        color: $white;
+    }
 </style>
