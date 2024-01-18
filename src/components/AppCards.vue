f1c5740d6d6123b76393281fe6f8803beaf3e18b
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-18 12:43:12 +0100

    Completata sezione pacchetti

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -75,81 +75,93 @@ export default {
                 <div class="d-flex justify-content-center">
 
                     <!-- Pacchetto Basic -->
-                    <div class="col-3 packages text-center">
+                    <div class="col-4">
+                        <div class="packages text-center">
+                            <div class="inner-card">
 
-                        <!-- Fronte della card -->
-                        <div class="front-card">
-                            <i class="fa-regular fa-handshake"></i>
-                            <h3 class="fw-bolder">Basic</h3>
-                            <p>Learn to ride a bike from the professionals</p>
-                            <ul>
-                                <li v-for="item, index in li_packs" :key="index">
-                                    <div class="d-flex align-items-center justify-content-center">
-                                        <i :class="item.done == true ? 'fa-solid fa-check' : 'fa-solid fa-xmark'"></i>
-                                        <span class="ms-2">{{item.name}}</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <!-- Retro della card -->
-                        <div class="retro-card">
-                            <h4 class="text-light text-uppercase fw-bolder">basic</h4>
-                            <p class="fw-bolder">$1800</p>
-                            <button class="fw-bolder">Send Request!</button>
+                                <!-- Fronte della card -->
+                                <div class="front-card">
+                                    <i class="fa-regular fa-handshake"></i>
+                                    <h3 class="fw-bolder">Basic</h3>
+                                    <p>Learn to ride a bike from the professionals</p>
+                                    <ul>
+                                        <li v-for="item, index in li_packs" :key="index">
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <i :class="item.done == true ? 'fa-solid fa-check' : 'fa-solid fa-xmark'"></i>
+                                                <span class="ms-2">{{item.name}}</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+        
+                                <!-- Retro della card -->
+                                <div class="retro-card">
+                                    <h4 class="text-light text-uppercase fw-bolder">basic</h4>
+                                    <p class="fw-bolder">$1800</p>
+                                    <button class="fw-bolder">Send Request!</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-
                     <!-- Pacchetto Standard -->
-                    <div class="col-3 packages text-center">
+                    <div class="col-4">
 
-                        <!-- Fronte della card -->
-                        <div class="front-card">
-                            <i class="fa-regular fa-handshake"></i>
-                            <h3 class="fw-bolder">Standard</h3>
-                            <p>Learn to ride a bike from the professionals</p>
-                            <ul>
-                                <li v-for="item, index in li_packs" :key="index">
-                                    <div class="d-flex align-items-center justify-content-center">
-                                        <i :class="item.done == true ? 'fa-solid fa-check' : 'fa-solid fa-xmark'"></i>
-                                        <span class="ms-2">{{item.name}}</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                        <div class="packages text-center">
+                            <div class="inner-card">
 
-                        <!-- Retro della card -->
-                        <div class="retro-card">
-                            <h4 class="text-light text-uppercase fw-bolder">Standard</h4>
-                            <p class="fw-bolder">$2500</p>
-                            <button class="fw-bolder">Send Request!</button>
+                                <!-- Fronte della card -->
+                                <div class="front-card">
+                                    <i class="fa-regular fa-handshake"></i>
+                                    <h3 class="fw-bolder">Standard</h3>
+                                    <p>Learn to ride a bike from the professionals</p>
+                                    <ul>
+                                        <li v-for="item, index in li_packs" :key="index">
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <i :class="item.done == true ? 'fa-solid fa-check' : 'fa-solid fa-xmark'"></i>
+                                                <span class="ms-2">{{item.name}}</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+        
+                                <!-- Retro della card -->
+                                <div class="retro-card">
+                                    <h4 class="text-light text-uppercase fw-bolder">Standard</h4>
+                                    <p class="fw-bolder">$2500</p>
+                                    <button class="fw-bolder">Send Request!</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
                     <!-- Pacchetto Premium -->
-                    <div class="col-3 packages text-center">
+                    <div class="col-4">
+                        <div class="packages text-center">
 
-                        <!-- Fronte della card -->
-                        <div class="front-card">
-                            <i class="fa-regular fa-handshake"></i>
-                            <h3 class="fw-bolder">Premium</h3>
-                            <p>Learn to ride a bike from the professionals</p>
-                            <ul>
-                                <li v-for="item, index in li_packs" :key="index">
-                                    <div class="d-flex align-items-center justify-content-center">
-                                        <i :class="item.done == true ? 'fa-solid fa-check' : 'fa-solid fa-xmark'"></i>
-                                        <span class="ms-2">{{item.name}}</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <!-- Retro della card -->
-                        <div class="retro-card">
-                            <h4 class="text-light text-uppercase fw-bolder">Premium</h4>
-                            <p class="fw-bolder">$5000</p>
-                            <button class="fw-bolder">Send Request!</button>
+                            <div class="inner-card">
+                                <!-- Fronte della card -->
+                                <div class="front-card">
+                                    <i class="fa-regular fa-handshake"></i>
+                                    <h3 class="fw-bolder">Premium</h3>
+                                    <p>Learn to ride a bike from the professionals</p>
+                                    <ul>
+                                        <li v-for="item, index in li_packs" :key="index">
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <i :class="item.done == true ? 'fa-solid fa-check' : 'fa-solid fa-xmark'"></i>
+                                                <span class="ms-2">{{item.name}}</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+        
+                                <!-- Retro della card -->
+                                <div class="retro-card">
+                                    <h4 class="text-light text-uppercase fw-bolder">Premium</h4>
+                                    <p class="fw-bolder">$5000</p>
+                                    <button class="fw-bolder">Send Request!</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -207,88 +219,98 @@ export default {
     .packages {
         height: 500px;
         margin: 30px;
-        position: relative;
+        perspective: 1000px;
 
-        &:hover .front-card {
-            display: none;
+        &:hover .inner-card {
+            transform: rotateY(180deg);
         }
 
-        &:hover .retro-card {
-            display: block;
-        }
-
-        .front-card {
-            display: block;
-            width: 100%;
+        .inner-card {
             height: 100%;
-            background-color: $black;
-            padding: 20px 0;
-            border-radius: 5px;
+            width: 100%;
+            position: relative;
+            transition: transform 0.6s;
+            transform-style: preserve-3d;
 
-            i {
-                color: $white;
-                font-size: 50px;
-            }
+            .front-card {
+                display: block;
+                width: 100%;
+                height: 100%;
+                background-image: linear-gradient(175deg, black 40%, white 40%);
+                box-shadow: 0 0 10px 5px $grey;
+                padding: 20px 0;
+                border-radius: 5px;
+                -webkit-backface-visibility: hidden;
+                backface-visibility: hidden;
 
-            h3 {
-                margin-top: 30px;
-                color: $white;
-                font-size: 20px;
-            }
+                i {
+                    color: $white;
+                    font-size: 50px;
+                }
 
-            p {
-                color: $white;
-                font-size: 15px;
-                margin-top: 15px;
-            }
+                h3 {
+                    margin-top: 30px;
+                    color: $white;
+                    font-size: 20px;
+                }
 
-            ul {
-                color: $white;
-                list-style: none;
-                margin-top: 80px;
-                padding: 0 30px;
-
-                li {
+                p {
+                    color: $white;
                     font-size: 15px;
-                    padding: 10px;
-                    margin: 10px 0;
-                    border-bottom: 1px solid grey;
+                    margin-top: 15px;
+                }
 
-                    i {
+                ul {
+                    list-style: none;
+                    margin-top: 80px;
+                    padding: 0 30px;
+
+                    li {
                         font-size: 15px;
+                        padding: 10px;
+                        margin: 10px 0;
+                        border-bottom: 1px solid lightgray;
+
+                        i {
+                            font-size: 15px;
+                            color: black;
+                        }
                     }
+                }
+            }
+
+            .retro-card {
+                background-color: $black;
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                padding: 40% 0;
+                border-radius: 5px;
+                transform: rotateY(180deg);
+                -webkit-backface-visibility: hidden;
+                backface-visibility: hidden;
+
+                h3 {
+                    margin: 40px;
+                }
+
+                p {
+                    color: $white;
+                    font-size: 40px;
+                }
+
+                button {
+                    margin-top: 50px;
+                    padding: 20px;
+                    background-color: black;
+                    color: $white;
+                    border-radius: 5px;
                 }
             }
         }
 
-        .retro-card {
-            background-color: $black;
-            position: absolute;
-            top: 0;
-            left: 0;
-            display: none;
-            width: 100%;
-            height: 100%;
-            padding: 40% 0;
-            border-radius: 5px;
-
-            h3 {
-                margin: 40px;
-            }
-
-            p {
-                color: $white;
-                font-size: 40px;
-            }
-
-            button {
-                margin-top: 50px;
-                padding: 20px;
-                background-color: black;
-                color: $white;
-                border-radius: 5px;
-            }
-        }
     }
 
 }
