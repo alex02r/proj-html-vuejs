1a1022846a770374c92c801b000deda764d8a6af
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-18 10:43:15 +0100

    add new class

--- src/components/jumbotron.vue
+++ src/components/jumbotron.vue
@@ -5,6 +5,7 @@ export default {
 </script>
 
 <template lang="">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <div class="mycontainer">
         <div class="myrow d-flex">
             <div class="col60 d-flex justify-content-end ">
@@ -14,11 +15,15 @@ export default {
             <div class="col40">
                 <h2 class="fw-bold">Do you want to be a professional cyclist?</h2>
                 <p>We offer 6 courses of varying difficulty and beautiful scenery that cyclists of all levels can enjoy. You will learn cycling from professionals with our competent and experienced staff. You will have a lot of fun with our amazing space.</p>
-                <h3 class="fw-bold">Professional Team</h3>
-                <p>We make you professional by adding your sincerity as well as professionalism.</p>
-                <h3 class="fw-bold">Professional Trainings</h3>
-                <p>We always ride with the best equipment, respecting nature and fun.</p>
-                <button>Get to know us</button>
+                <div class="Professional-team">
+                    <h3 class="fw-bold">Professional Team</h3>
+                    <p>We make you professional by adding your sincerity as well as professionalism.</p>
+                </div>
+                <div class="Professional-trainings">
+                    <h3 class="fw-bold">Professional Trainings</h3>
+                    <p>We always ride with the best equipment, respecting nature and fun.</p>
+                </div>
+                <button class="d-flex alin align-items-center">Get to know us <i class="fa-solid fa-arrow-right p-2"></i></button>
             </div>
         </div>
         
@@ -57,7 +62,7 @@ export default {
 
 
 button {
-    padding: 20px;
+    padding: 20px 50px;
     font-weight: bold;
     background-color: black;
     color: white;
