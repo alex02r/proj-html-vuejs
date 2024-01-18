edf34b6c47d604aceb5974fd006ca73c6f835f02
alex02r <alex.giochi02@gmail.com>
2024-01-18 12:49:09 +0100

    [FIX] Sisteamto l'import di MOtion plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
+import { MotionPlugin } from '@vueuse/motion'
 import './style.css'
 import App from './App.vue'
-import { MotionPlugin } from '@vueuse/motion'
-
-createApp(App).mount('#app')
+createApp(App).use(MotionPlugin).mount('#app')
