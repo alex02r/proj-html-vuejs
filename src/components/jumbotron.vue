bc2fca17179101da38912884cdf841b289c931ac
alex02r <alex.giochi02@gmail.com>
2024-01-19 11:28:33 +0100

    [FIX] Aggiunto un po di spazio nelle aree del jumbo

--- src/components/jumbotron.vue
+++ src/components/jumbotron.vue
@@ -15,22 +15,24 @@ export default {
                 </div>
             </div>
             <div class="col-4">
-                <h2 class="fw-bold">Do you want to be a professional cyclist?</h2>
-                <p>We offer 6 courses of varying difficulty and beautiful scenery that cyclists of all levels can enjoy. You will learn cycling from professionals with our competent and experienced staff. You will have a lot of fun with our amazing space.
-                    <br>
-                    <img src="../assets/divider.jpg" alt="divider" class="w-25">
-                </p>
+                <div class="title">
+                    <h2>Do you want to be a professional cyclist?</h2>
+                    <p>We offer 6 courses of varying difficulty and beautiful scenery that cyclists of all levels can enjoy. You will learn cycling from professionals with our competent and experienced staff. You will have a lot of fun with our amazing space.
+                        <br>
+                        <img src="../assets/divider.jpg" alt="divider" class="w-25">
+                    </p>
+                </div>
                 <div class="paragraph">
                     <img class="icon" src="https://cdn-icons-png.flaticon.com/128/882/882115.png"  alt="">
                     <div>
-                        <h3 class="fw-bold">Professional Team</h3>
+                        <h3>Professional Team</h3>
                         <p>We make you professional by adding your sincerity as well as professionalism.</p>
                     </div>
                 </div>
                 <div class="paragraph">
                     <img class="icon" src="https://cdn-icons-png.flaticon.com/128/972/972998.png" alt="">
                     <div>
-                        <h3 class="fw-bold">Professional Trainings</h3>
+                        <h3>Professional Trainings</h3>
                         <p>We always ride with the best equipment, respecting nature and fun.</p>
                     </div>
                 </div>
@@ -64,14 +66,26 @@ export default {
 }
 button {
     padding: 20px 50px;
-    font-weight: bold;
+    font-size: 18px;
+    font-weight: 800;
     background-color: $black;
     color: $white;
+}
+.title{
+    h2{
+        font-weight: 800;
+    }
+    p{
+        padding: 10px 0;
+    }
 }
 .paragraph{
     display: flex;
     align-items: center;
     gap: 20px;
+    h3{
+        font-weight: 600;
+    }
     .icon{
         width: 70px;
         height: 70px;
